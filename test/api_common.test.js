fde0ba534b3f7cb52de700d506a92388d611a72c
yybs2015 <liwenyue@live.cn>
2018-05-26 19:34:43 +0800

    添加简单的测试

--- test/api_common.test.js
+++ test/api_common.test.js
@@ -1,30 +1,52 @@
 'use strict';
 
-const API = require('../');
+const {API, ComponentAPI, Oauth} = require('../');
 const expect = require('expect.js');
 const config = require('./config');
 
 describe('api_common', function () {
   describe('isAccessTokenValid', function () {
     it('should invalid', function () {
-      var token = new API.AccessToken('token', new Date().getTime() - 7200 * 1000);
+      let token = new API.AccessToken('token', new Date().getTime() - 7200 * 1000);
       expect(token.isValid()).not.to.be.ok();
     });
 
     it('should valid', function () {
-      var token = new API.AccessToken('token', new Date().getTime() + 7200 * 1000);
+      let token = new API.AccessToken('token', new Date().getTime() + 7200 * 1000);
       expect(token.isValid()).to.be.ok();
     });
   });
 
   describe('isComponentAccessTokenValid', function () {
     it('should invalid', function () {
-      var token = new API.ComponentAccessToken('componentAccessToken', new Date().getTime() - 7200 * 1000);
+      let token = new ComponentAPI.ComponentAccessToken('componentAccessToken', new Date().getTime() - 7200 * 1000);
       expect(token.isValid()).not.to.be.ok();
     });
 
     it('should valid', function () {
-      var token = new API.ComponentAccessToken('componentAccessToken', new Date().getTime() + 7200 * 1000);
+      let token = new ComponentAPI.ComponentAccessToken('componentAccessToken', new Date().getTime() + 7200 * 1000);
+      expect(token.isValid()).to.be.ok();
+    });
+  });
+
+  describe('isUserTokenValid', function () {
+    it('should invalid', function () {
+      let data = {
+        create_at: new Date().getTime() - 7200 * 1000,
+        expires_in: 7200,
+        access_token: 'userAccessToken'
+      };
+      let token = new Oauth.UserAccessToken(data);
+      expect(token.isValid()).not.to.be.ok();
+    });
+
+    it('should valid', function () {
+      let data = {
+        create_at: new Date().getTime() + 7200 * 1000,
+        expires_in: 7200,
+        access_token: 'userAccessToken'
+      };
+      let token = new Oauth.UserAccessToken(data);
       expect(token.isValid()).to.be.ok();
     });
   });
@@ -36,51 +58,188 @@ describe('api_common', function () {
     });
 
     it('should not ok when override method', function () {
-      var obj = {sayHi: function () {}};
+      let obj = {sayHi: function () {}};
       expect(API.mixin).withArgs(obj).to.throwException(/Don't allow override existed prototype method\./);
     });
   });
 
-
-  describe('getComponentAccessToken', function () {
-    it('should ok', async function () {
-      var api = new API(config.appid, config.appsecret);
-      var token = await api.getComponentAccessToken();
-      expect(token).to.only.have.keys('componentAccessToken', 'expireTime');
+  
+  describe('createComponentApi', function () {
+    it('should not ok create with not any props', async function () {
+      try {
+        let oauth = new Oauth({
+          authorizerAppId: config.authorizerAppId,
+          getUserToken: async function() {
+            return {};
+          },
+          saveUserToken: async function(token) {
+  
+          }
+        });
+      } catch (err) {
+        expect(err.message).to.equal('参数不完整');
+      }
+    });
+    it('should have ensureComponentToken', async function () {
+      let api = new ComponentAPI({
+        componentAppId: config.componentAppId,
+        componentAppSecret: config.componentAppSecret,
+        getComponentTicket: async function() {
+          return config.componentTicket;
+        },
+        getComponentToken: async function() {
+          return {accessToken: '', expireTime: ''};
+        },
+        saveComponentToken: async function(token) {
+          
+        }
+      });
+      expect(api).to.have.property('ensureComponentToken');
     });
 
-    it('should not ok with invalid appid', async function () {
-      var api = new API('appid', 'secret', 'error');
+    it('should not ok create with not componentAPI', async function () {
       try {
-        await api.getComponentAccessToken();
+        let oauth = new Oauth({
+          authorizerAppId: config.authorizerAppId,
+          getUserToken: async function() {
+            return {};
+          },
+          saveUserToken: async function(token) {
+  
+          }
+        });
       } catch (err) {
-        expect(err).to.have.property('name', 'WeChatAPIError');
-        expect(err).to.have.property('message');
-        expect(err.message).to.match(/invalid appid/);
+        expect(err.message).to.equal('参数不完整');
       }
     });
 
-    it('should not ok with invalid appsecret', async function () {
-      var api = new API(config.appid, 'appsecret');
+    it('should ok create with componentAPI', async function () {
+      let componentApi = new ComponentAPI({
+        componentAppId: config.componentAppId,
+        componentAppSecret: config.componentAppSecret,
+        getComponentTicket: async function() {
+          return 'getComponentTicket';
+        },
+        getComponentToken: async function() {
+          return {accessToken: '', expireTime: ''};
+        },
+        saveComponentToken: async function(token) {
+          
+        }
+      });
+      expect(componentApi).to.have.property('component_appid', config.componentAppId);
+      expect(componentApi).to.have.property('component_appsecret', config.componentAppSecret);
+      expect(componentApi).to.have.keys('ensureComponentToken','getComponentTicket', 'getComponentToken', 'saveComponentToken');
+    });
+
+  });
+
+  describe('createOauth', function () {
+    it('should not ok create with not componentAPI', async function () {
       try {
-        await api.getComponentAccessToken();
+        let oauth = new Oauth({
+          authorizerAppId: config.authorizerAppId,
+          getUserToken: async function() {
+            return {};
+          },
+          saveUserToken: async function(token) {
+  
+          }
+        });
       } catch (err) {
-        expect(err).to.have.property('name', 'WeChatAPIError');
-        expect(err).to.have.property('message');
-        expect(err.message).to.match(/invalid appsecret/);
+        expect(err.message).to.equal('参数不完整');
       }
+    });
+
+    it('should ok create with componentAPI', async function () {
+      let componentApi = new ComponentAPI({
+        componentAppId: config.componentAppId,
+        componentAppSecret: config.componentAppSecret,
+        getComponentTicket: async function() {
+          return 'getComponentTicket';
+        },
+        getComponentToken: async function() {
+          return {accessToken: '', expireTime: ''};
+        },
+        saveComponentToken: async function(token) {
+          
+        }
+      });
+      let oauth = new Oauth({
+        authorizerAppid: config.authorizerAppid,
+        getUserToken: async function() {
+          return {};
+        },
+        saveUserToken: async function(token) {
+
+        },
+        componentApi
+      });
+      expect(oauth).to.have.property('component_appid', config.componentAppId);
+      expect(oauth).to.have.property('component_appsecret', config.componentAppSecret);
+      expect(oauth).to.have.keys('ensureComponentToken','getUserToken', 'saveUserToken', 'componentApi');
+      expect(oauth).to.have.property('componentApi',componentApi);
+    });
+  });
+
+
+  describe('createAPI', function () {
+    it('should not ok create with not componentAPI', async function () {
+      try {
+        let api = new API({
+          authorizerAppId: config.authorizerAppId,
+          getToken: async function() {
+            return {};
+          },
+          saveToken: async function(token) {
+  
+          }
+        });
+      } catch (err) {
+        expect(err.message).to.equal('参数不完整');
+      }
+    });
+
+    it('should ok create with componentAPI', async function () {
+      let componentApi = new ComponentAPI({
+        componentAppId: config.componentAppId,
+        componentAppSecret: config.componentAppSecret,
+        getComponentTicket: async function() {
+          return 'getComponentTicket';
+        },
+        getComponentToken: async function() {
+          return {accessToken: '', expireTime: ''};
+        },
+        saveComponentToken: async function(token) {
+          
+        }
+      });
+      let api = new API({
+        authorizerAppid: config.authorizerAppid,
+        getUserToken: async function() {
+          return {};
+        },
+        saveUserToken: async function(token) {
+
+        },
+        componentApi
+      });
+      expect(api).to.have.property('component_appid', config.componentAppId);
+      expect(api).to.have.property('component_appsecret', config.componentAppSecret);
+      expect(api).to.have.keys('ensureComponentToken','getToken', 'saveToken', 'componentApi');
+      expect(api).to.have.property('componentApi',componentApi);
     });
   });
 
   describe('getAccessToken', function () {
     it('should ok', async function () {
-      var api = new API(config.appid, config.appsecret,config.authorizerappid);
-      var token = await api.getAccessToken();
+      let api = new API(config.appid, config.appsecret,config.authorizerappid);
+      let token = await api.getAccessToken();
       expect(token).to.only.have.keys('accessToken', 'expireTime');
     });
 
     it('should not ok with invalid appid', async function () {
-      var api = new API('appid', 'secret');
+      let api = new API('appid', 'secret');
       try {
         await api.getAccessToken();
       } catch (err) {
@@ -91,7 +250,7 @@ describe('api_common', function () {
     });
 
     it('should not ok with invalid appsecret', async function () {
-      var api = new API(config.appid, config.appsecret);
+      let api = new API(config.appid, config.appsecret);
       try {
         await api.getAccessToken();
       } catch (err) {
--- test/config.js
+++ test/config.js
@@ -1,9 +1,8 @@
 'use strict';
 
 module.exports = {
-  appid: 'wxc9135aade4e81d57',
-  appsecret: '0461795e98b8ffde5a212b5098f1b9b6',
-  authorizerappid: 'wxc9135aade4e81d53'
-  menuid: '208379533',
-  user_id: 'nick'
+  componentAppId: 'wxc9135aade4e81d57',
+  componentAppSecret: '0461795e98b8ffde5a212b5098f1b9b6',
+  authorizerAppId: 'wxc9135aade4e81d53',
+  componentTicket: 'componentTicket'
 };
